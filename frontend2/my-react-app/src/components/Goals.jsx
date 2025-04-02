789708efaa1d0c251dde3f0ed6cbbf2640f7eea6
X <fancymngmt@gmail.com>
2025-04-02 15:21:59 -0400

    frontend

---
+++ frontend2/my-react-app/src/components/Goals.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Goals() {
+  return (
+    <div className="page-container">
+      <h2>Goals</h2>
+      {/* Add your goals content here */}
+    </div>
+  )
+}
+
+export default Goals 
